411f56f4cd4ba9c7af9d476732a7b497be6f8d6c
blusque <kokutouaikata@outlook.com>
2025-01-20 20:07:49 +0800

    map location bug fixed

--- run_env.py
+++ run_env.py
@@ -167,7 +167,7 @@ def run(rank, num_procs, args):
     if trained_model_path:
         print('Loading model param:{}\n model config:{}'.format(trained_model_path, model_config_file))
         model = model_builder.build_model(model_config_file, dataset, device)
-        state_dict = torch.load(trained_model_path)
+        state_dict = torch.load(trained_model_path, map_location=device)
         model.load_state_dict(state_dict)
         # try:
         #     print('Loading model param:{}\n model config:{}'.format(trained_model_path, model_config_file))
